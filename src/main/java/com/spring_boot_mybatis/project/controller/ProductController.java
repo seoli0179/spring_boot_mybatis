575190fa94b01cc7d7b0ba701d76667e032893c7
seoli0179 <seoli0179@kakao.com>
2022-09-13 15:48:31 +0900

    webhook test

--- src/main/java/com/spring_boot_mybatis/project/controller/ProductController.java
+++ src/main/java/com/spring_boot_mybatis/project/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//test
+//test1
 @Controller
 public class ProductController {
 	
